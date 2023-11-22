430c0b8242451cca1639eee494ef4d7cbd0cc9df
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-22 17:40:59 +0200

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/Untitled.png"
+
+const Header = () =>{
+    return (
+        <header>
+            <img src={logo} alt="lll" />
+            <h1>Investment calaculator</h1>
+        </header>
+    )
+}
+export default Header
+
